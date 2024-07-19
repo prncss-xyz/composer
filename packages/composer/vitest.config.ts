f63a09ca1a34d6c5612ff0cc701e237a71e1c758
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-07-19 18:13:24 -0400

    refactor: config tweaks

--- packages/composer/vitest.config.ts
+++ packages/composer/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		include: ['src/**'],
+		include: ['src/**/*.test.{js,ts,jsx,tsx}'],
 		coverage: {
 			reporter: ['text', 'json', 'clover', 'lcov'],
 		},
